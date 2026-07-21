822e6d8aefd58e4a15a5b34d0a9605d36c3105dc
Eric Joldasov <bratishkaerik@landless-city.net>
2026-07-21 09:36:55 +0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
+    repositories {
+        mavenCentral()
+    }
+}
+
+rootProject.name = "zigocracy"
