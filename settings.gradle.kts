6bd8dfe1d7a060e877b42e597b2ad177271d76ca
Eric Joldasov <bratishkaerik@landless-city.net>
2026-07-21 10:57:16 +0500

    chore: add .editorconfig` for Kotlin source files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+	id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-    }
+	@Suppress("UnstableApiUsage")
+	repositories {
+		mavenCentral()
+	}
 }
 
 rootProject.name = "zigocracy"
